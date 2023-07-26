0bbb8e09dd8c663d5088e7633d9ee74d2b0a1939
Sundram Yadav <hot2shortcut@gmail.com>
2023-07-26 20:34:18 +0530

    added another test case

--- cypress/e2e/loginAuthentication.spec.ts
+++ cypress/e2e/loginAuthentication.spec.ts
@@ -5,7 +5,7 @@ describe("Login and Authentications Flows", function () {
     });
     cy.visit("https://practicetestautomation.com/practice-test-login/");
   });
-  it("TestID-1_Verify that a user with valid credentials can log in and log out successfully.", function () {
+  it("TestID1_Verify that a user with valid credentials can log in and log out successfully.", function () {
     const jsondata = this.data;
     // To validate the title of the page
     cy.title().should("eq", "Test Login | Practice Test Automation");
@@ -19,13 +19,15 @@ describe("Login and Authentications Flows", function () {
     cy.findByRole("textbox", { name: "Username" }).type(jsondata.userid);
     cy.get("#password").type(jsondata.password);
     cy.findByRole("button", { name: "Submit" }).click();
+    // Validating URL
+    cy.url().should("include", "logged-in-successfully/");
     cy.contains("Logged In Successfully").should("be.visible");
     cy.contains("Congratulations student. You successfully logged in!").should(
       "be.visible"
     );
     cy.findByRole("link", { name: "Log out" }).click();
   });
-  it("TestID-2_Verify that incorrect username and password result in login failure.", function () {
+  it("TestID2_Verify that incorrect username and password result in login failure.", function () {
     const jsondata = this.data;
     // To validate the title of the page
     cy.title().should("eq", "Test Login | Practice Test Automation");
@@ -51,5 +53,13 @@ describe("Login and Authentications Flows", function () {
     cy.get("#error").within(() => {
       cy.contains("Your password is invalid!").should("be.visible");
     });
+  });
+  it("TestID3_should lock the account after multiple failed login attempts", () => {
+    //Since there is no such practice website I have just written the logic
+    for (let i = 0; i < 5; i++) {
+      cy.get("[data-testid=username]").type("testuser");
+      cy.get("[data-testid=password]").type("wrongpassword");
+      cy.get("[data-testid=login-button]").click();
+    }
   });
 });
